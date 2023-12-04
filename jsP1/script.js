5c2ad403816bfcd381a41fc00631dc5077ab0570
snehasharmaax <snehasharmaax@gmail.com>
2023-12-04 14:38:33 +0530

    Add JS file

---
+++ jsP1/script.js
@@ -0,0 +1,25 @@
+a = 10;
+b = 5;
+age = 23;
+name = "Sneha";
+console.log(a + b);
+present = true;
+
+console.log(typeof a);
+console.log(typeof name);
+console.log(typeof present);
+
+c = 4.5;
+console.log(typeof c);
+
+price = -99.99;
+console.log(typeof price);
+
+op1 = (2 + 1) * 3;
+console.log(op1);
+
+op2 = 3 / 1 + 2 ** 2;
+console.log(op2);
+
+op3 = 4 + (1 * 6) / 2;
+console.log(op3);
