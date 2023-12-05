e6d21789630e7b0a441429e265241e093d3200be
snehasharmaax <snehasharmaax@gmail.com>
2023-12-05 22:36:33 +0530

    Start JS Part 2

---
+++ jsP2/script.js
@@ -0,0 +1,5 @@
+// Template Literals
+let pencilPrice = 10;
+let eraseerPrice = 5;
+// console.log("The total price is :", pencilPrice + eraseerPrice, "Rupees");
+console.log(`The total price is ${pencilPrice + eraseerPrice} Rupees.`); // Template Literals
