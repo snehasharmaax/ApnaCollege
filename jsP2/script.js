6f9393af8d660ea520e8a9191c74b55c1e5b1714
snehasharmaax <snehasharmaax@gmail.com>
2023-12-06 22:35:58 +0530

    Modified File

--- jsP2/script.js
+++ jsP2/script.js
@@ -1,5 +1,43 @@
-// Template Literals
-let pencilPrice = 10;
-let eraseerPrice = 5;
-// console.log("The total price is :", pencilPrice + eraseerPrice, "Rupees");
-console.log(`The total price is ${pencilPrice + eraseerPrice} Rupees.`); // Template Literals
+// console.log("This is a simple msg");
+// console.error("This is an error msg");
+// console.warn("This is a warn msg");
+
+// // Template Literals
+// let pencilPrice = 10;
+// let eraseerPrice = 5;
+// // console.log("The total price is :", pencilPrice + eraseerPrice, "Rupees");
+// console.log(`The total price is ${pencilPrice + eraseerPrice} Rupees.`); // Template Literals
+
+// if (" ") {
+//   console.log("String is empty ");
+// } else {
+//   console.log("String is not empty");
+// }
+// let num = 0;
+// if (num) {
+//   console.log("num is not equal to 0");
+// } else {
+//   console.log("num is equal to 0");
+// }
+
+// let color = "white";
+// switch (color) {
+//   case "red":
+//     console.log("stop");
+//     break;
+//   case "yellow":
+//     console.log("slow dowm");
+//     break;
+//   case "green":
+//     console.log("GO");
+//     break;
+//   default:
+//     console.log("Broken light");
+// }
+
+// alert("Something is wrong ");
+
+let fName = prompt("Enter your first name");
+let lName = prompt("Enter your last name");
+let msg = `Welcome ${fName} ${lName}`;
+alert(msg);
