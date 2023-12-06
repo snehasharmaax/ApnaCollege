37c468d2f9828c77290350c166ff40f0555c4d12
snehasharmaax <snehasharmaax@gmail.com>
2023-12-06 22:35:09 +0530

    Practice Question

---
+++ jsP2/PQs.js
@@ -0,0 +1,87 @@
+// ==========================Practice Question 1=====================
+// let color = "white";
+// // Traffic Light Sustem
+// if (color == "red") {
+//   console.log("Stop! light color is red");
+// } else if (color == "yellow") {
+//   console.log(" Slow down! light color is yellow");
+// } else if (color == "green") {
+//   console.log("Go! light color is green");
+// } else {
+//   console.log("Traffic light is broken!");
+// }
+
+// ==========================Example============================
+// let marks = 24;
+// if (marks >= 80) {
+//   console.log("A+");
+// } else if (marks >= 60) {
+//   console.log("A");
+// } else if (marks >= 33) {
+//   console.log("B");
+// } else if (marks < 33) {
+//   console.log("Fail");
+// }
+
+// let age = 14;
+// if (age >= 18) {
+//   console.log("You can vote");
+// } else {
+//   console.log("You cannot vote");
+// }
+
+// ===========================Practice Question 2 ===========================
+// let size = "XL";
+// if (size === "XL") {
+//   console.log("Price is Rs. 250");
+// } else if ("L") {
+//   console.log("Price is Rs. 200");
+// } else if ("M") {
+//   console.log("Price is Rs. 100");
+// } else {
+//   console.log("Price is Rs. 50");
+// }
+
+// ===========================Practice Question 3.1 ============================
+// let str = "akash";
+// if (str[0] === "a" && str.length > 3) {
+//   console.log("Good string");
+// } else {
+//   console.log("Not a Good String");
+// }
+// ===========================Practice Question 3.2 ============================
+// let num = 12;
+// if (num % 3 === 0 && (num + 1 == 15 || num - 1 == 11)) {
+//   console.log("Safe");
+// } else {
+//   console.log("Unsafe");
+// }
+
+// ===========================Practice Question 4 ===============================
+let day = 5;
+switch (day) {
+  case 1:
+    console.log("It is Monday");
+    break;
+  case 2:
+    console.log("It is Tuesday");
+    break;
+  case 3:
+    console.log("It is Wednesday");
+    break;
+  case 4:
+    console.log("It is Thursday");
+    break;
+  case 5:
+    console.log("It is Friday");
+    break;
+  case 6:
+    console.log("It is Saturday");
+    break;
+  case 7:
+    console.log("It is Sunday");
+    break;
+  default:
+    console.log("Invalid Choice");
+    break;
+}
