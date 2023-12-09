1036da84a5f45bb66b446dd1daf078eb0bd28da7
snehasharmaax <snehasharmaax@gmail.com>
2023-12-09 23:58:48 +0530

    Javascript Part 3

---
+++ jsP3/array.js
@@ -0,0 +1,15 @@
+// let students = ["sneha", "sanjna", "sudha", "riya", "ashi"];
+// console.log(students);
+// console.log(students[0]);
+// console.log(students.length);
+// console.log(students[2][0]);
+// console.log(students[2].length);
+
+// let marks = [30, 50, 60, 70, 40];
+// let mixed = ["sneha", 60, "science", true];
+// let empty = [];
+// console.log(marks, mixed, empty);
+
+// =================== Arrays are mutable ======================
+let name = "lucky";
+console.log(name[0]);
