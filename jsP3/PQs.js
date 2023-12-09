696a9c3374fc4984268de6129d5e4759f117eac4
snehasharmaax <snehasharmaax@gmail.com>
2023-12-09 23:59:28 +0530

    Javascript Part 3

---
+++ jsP3/PQs.js
@@ -0,0 +1,15 @@
+// Question 1
+// let msg = "hello!";
+// console.log(msg.trim().toUpperCase());
+
+// Question 2
+// let name = "SnehaSharma";
+// console.log(name.slice(5, 9));
+// console.log(name.indexOf("Sh"));
+// console.log(name.replace("Sneha", "Sanjna"));
+
+// Question 3
+let college = "apnacollege";
+console.log(college);
+console.log(college.slice(4, college.length));
+console.log(college.replace("l", "t").replace("l", "t"));
