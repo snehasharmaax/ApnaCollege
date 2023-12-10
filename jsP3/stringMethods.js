5d5a69d12fce4e2bce04423817284a264df8c82b
snehasharmaax <snehasharmaax@gmail.com>
2023-12-10 23:28:27 +0530

    String Method

---
+++ jsP3/stringMethods.js
@@ -0,0 +1,43 @@
+// ============================ trim ==============================
+// let msg = "  Hello  ";
+// console.log(msg);
+// console.log(msg.trim());
+
+// let password = prompt("Set your password");
+// console.log(password);
+// console.log(password.trim());
+
+// ============================ Upper case, Lower case =================
+// let name = "Apna College";
+// console.log(name);
+// console.log(name.toUpperCase());
+// console.log(name.toLowerCase());
+
+// =========================== indexOf ===============================
+// let msg = "ILoveCoding";
+// console.log(msg);
+// console.log(msg.indexOf("Love"));
+// console.log(msg.indexOf("Coding"));
+// console.log(msg.indexOf("e"));
+// console.log(msg.indexOf("Sne"));
+
+// =========================== method chaining =======================
+// let msg = "     Sneha       ";
+// console.log(msg);
+// console.log(msg.toUpperCase().trim());
+
+// =========================== slice method ==========================
+// let msg = "ILoveCoding";
+// console.log(msg);
+// console.log(msg.slice(1, 5));
+// console.log(msg.slice(5, msg.length));
+
+// =========================== replace method ========================
+// let msg = "ILoveCoding";
+// console.log(msg);
+// console.log(msg.replace("Love", "Do"));
+
+// ============================ repeat method =======================
+// let msg = "ILoveCoding";
+// console.log(msg);
+// console.log(msg.repeat(4));
