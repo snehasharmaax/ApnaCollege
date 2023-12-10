11894f6fa5153c241b4729f84ad304905760ed2f
snehasharmaax <snehasharmaax@gmail.com>
2023-12-10 23:31:52 +0530

    Javascript Part 4

--- jsP4/loops.js
+++ jsP4/loops.js
@@ -2,7 +2,9 @@
 //   console.log(i);
 // }
 
-for (var i = 5; i >= 1; i--) {
-  console.log(i);
-}
-console.log(i);
+// for (var i = 5; i >= 1; i--) {
+//   console.log(i);
+// }
+// console.log(i);
+
+// Print all 1 to 15
