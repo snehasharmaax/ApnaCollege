10a2d3a0962b436c511a7bbe854fde85b1b42dd8
snehasharmaax <snehasharmaax@gmail.com>
2023-12-10 23:27:04 +0530

    Assignment

---
+++ jsP3/Assignment.js
@@ -0,0 +1,37 @@
+// Question 1
+// let arr = [7, 9, 0, -2];
+// console.log(arr.slice(0, 3));
+
+// Question 2
+// let arr2 = [7, 9, 0, -2];
+// console.log(arr.slice(1, arr.length));
+
+// Question 3
+// let str = prompt("Please enter a string");
+// if (str.length == 0) {
+//   console.log("String is empty");
+// } else {
+//   console.log("String is not empty ");
+// }
+
+// Question 4
+// let str = "SnEhAsHaRmA";
+// if (str[4] === str[4].toLowerCase()) {
+//   console.log("Character is Lowercase");
+// } else {
+//   console.log("Character is not lowercase");
+// }
+
+// Question 5
+// let str = prompt("Please enter a string ");
+// console.log(`Original String = ${str}`);
+// console.log(`String without string = ${str.trim()}`);
+
+// Question 6
+let arr = ["hello ", "a", 23, 64, 99, -6];
+// let item = 64;
+if (arr.indexOf(item) != 23) {
+  console.log(`Element exists in array`);
+} else {
+  console.log(`Element doesn't exists in array`);
+}
