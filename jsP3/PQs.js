35bbe7d21c7418e1fbf8c9cd3e3731b1083179cc
snehasharmaax <snehasharmaax@gmail.com>
2023-12-10 23:29:29 +0530

    Javascript Part 4

--- jsP3/PQs.js
+++ jsP3/PQs.js
@@ -1,3 +1,4 @@
+// ------------------ Practice Question 1 --------------------
 // Question 1
 // let msg = "hello!";
 // console.log(msg.trim().toUpperCase());
@@ -9,7 +10,40 @@
 // console.log(name.replace("Sneha", "Sanjna"));
 
 // Question 3
-let college = "apnacollege";
-console.log(college);
-console.log(college.slice(4, college.length));
-console.log(college.replace("l", "t").replace("l", "t"));
+// let college = "apnacollege";
+// console.log(college);
+// console.log(college.slice(4, college.length));
+// console.log(college.replace("l", "t").replace("l", "t"));
+
+// ------------------- Practice Question 2 -------------------
+// Question 4
+// let months = ["jan", "july", "march", "august"];
+// console.log(months);
+// let remMon = months.shift();
+// console.log(months);
+// remMon = months.shift();
+// console.log(months);
+
+// let addMon = months.unshift("june");
+// console.log(months);
+// addMon = months.unshift("july");
+// console.log(months);
+
+// ------------------- Practice Question 3 -------------------
+// Question 5
+// let months = ["jan", "july", "march", "august"];
+// months.splice(0, 2, "july", "june");
+
+// Question 6
+// let lang = ["c", "c++", "html", "javascript", "python", "java", "c#", "sql"];
+// let ans = lang.reverse().indexOf("javascript");
+// console.log(ans);
+
+// ------------------- Practice Question 3 -------------------
+// Question 7
+let ttt = [
+  ["X", null, "O"],
+  [null, "X", null],
+  ["O", null, "X"],
+];
+console.log(ttt);
--- jsP3/array.js
+++ jsP3/array.js
@@ -11,5 +11,103 @@
 // console.log(marks, mixed, empty);
 
 // =================== Arrays are mutable ======================
-let name = "lucky";
-console.log(name[0]);
+// let name = "lucky";
+// console.log(name[0]);
+
+// let fruits = ["mango", "apple", "litchi"];
+// console.log(fruits);
+// fruits[1] = "banana";
+// console.log(fruits);
+
+// fruits[10] = "pineapple";
+// console.log(fruits);
+
+// ========================= Array Method ==================
+// Push
+// console.log("Push Method");
+// let cars = ["audi", "toyota", "BMW", "maruti"];
+// console.log(cars);
+// let addCar = cars.push("XUV");
+// console.log(cars);
+// addCar = cars.push("ferrari");
+// console.log(cars);
+
+// Pop
+// console.log("Pop Method");
+// removeCar = cars.pop();
+// console.log(cars);
+// console.log(removeCar);
+
+// Unshift
+// console.log("Unshift");
+// let addStart = cars.unshift("Bolero");
+// console.log(cars);
+// addStart = cars.unshift("Baleno");
+// console.log(cars);
+
+// Shift
+// console.log("Shift");
+// let delStart = cars.shift("Baleno");
+// console.log(cars);
+
+// indexOf
+// let marks = [99, 44, 69, 39, 95];
+// console.log(marks.indexOf(39));
+// console.log(marks.indexOf(93));
+
+// includes
+// let data = ["sneha", 20, "Computer Science", true, "Female"];
+// console.log(data.includes(20));
+
+// concat
+// let primary = ["red", "yellow", "blue"];
+// let secondary = ["orange", "green", "violet"];
+// let allColors = primary.concat(secondary);
+// console.log(allColors);
+
+// reverse
+// let cars = ["audi", "toyota", "BMW", "maruti"];
+// console.log(cars.reverse());
+
+// slice
+// let colors = ["red", "yellow", "blue", "orange", "pink", "white"];
+// console.log(colors.slice());
+// console.log(colors.slice(2));
+// console.log(colors.slice(2, 3));
+// console.log(colors.slice(-2));
+
+// splice
+// let colors = ["red", "yellow", "blue", "orange", "pink", "white"];
+// console.log(colors.splice(3));
+// console.log(colors.splice(0, 1));
+// console.log(colors.push("grey", "skyblue", "black", "brown", "wheat"));
+// console.log(colors.splice(1, 2));
+// console.log(colors.splice(0, 0, "green", "pink", "tealgreen"));
+// console.log(colors.splice(1, 0, "purple"));
+
+// sort
+// let colors = ["red", "yellow", "blue", "orange", "pink", "white"];
+// colors.sort();
+// let chars = ["b", "d", "c", "a"];
+// console.log(chars.sort());
+
+// reference
+// let arr = ["a", "b"];
+// let arrCopy = arr;
+// arrCopy.push("c");
+// let compare = arr == arrCopy;
+// console.log(compare);
+
+// const variable
+// const pi = 3.14;
+
+// nestede arrays
+let nums = [
+  [2, 4],
+  [3, 6],
+  [4, 8],
+];
+console.log(nums);
+console.log(nums[1]);
+console.log(nums[1][0]);
+console.log(nums[1].length);
