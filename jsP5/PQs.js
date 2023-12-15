2c038b26d7eb6952eff4da9b59c64db7f97085c9
snehasharmaax <snehasharmaax@gmail.com>
2023-12-15 22:31:34 +0530

    Practice Questions

---
+++ jsP5/PQs.js
@@ -0,0 +1,8 @@
+// Qs1 - Generate a random number between 1 and 100
+let pq = Math.floor(Math.random() * 100) + 1;
+
+// Qs1 - Generate a random number between 1 and 5
+let pq2 = Math.floor(Math.random() * 5) + 1;
+
+// Qs1 - Generate a random number between 21 and 25
+let pq3 = Math.floor(Math.random() * 5) + 21;
