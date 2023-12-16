17186d4e21589bd6527b3607bcfd36ef6de6409c
snehasharmaax <snehasharmaax@gmail.com>
2023-12-16 20:45:34 +0530

    Assignment

---
+++ jsP5/Assignment.js
@@ -0,0 +1,21 @@
+// Question 1
+let dice = Math.floor(Math.random() * 6) + 1;
+console.log(dice);
+
+// Question 2
+let car = {
+  name: "Thar",
+  model: "S23",
+  color: "red",
+};
+console.log(car["name"]);
+
+// Question 3
+let person = {
+  name: "Sneha",
+  age: 21,
+  city: "Aligarh",
+};
+person.city = "New York";
+person.country = "United States";
+console.log(person);
