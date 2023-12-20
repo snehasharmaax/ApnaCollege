9c3d47efc182f9103911f72d788f51c2442d9ea3
snehasharmaax <snehasharmaax@gmail.com>
2023-12-20 23:11:00 +0530

    Javascript Partr 6

--- jsP6/funcExp.js
+++ jsP6/funcExp.js
@@ -1,6 +1,49 @@
 // ---------------------------- Function Expression ----------------------------------
-let name = "sneha";
-let sum = function (a, b) {
-  return a + b;
+// let name = "sneha";
+// let sum = function (a, b) {
+//   return a + b;
+// };
+// console.log(sum);
+
+// ---------------------------- Higher Order Functions
+// function multipleGreet(func, count) {
+//higher order function
+//   for (let i = 1; i <= count; i++) {
+//     func();
+//   }
+// }
+
+// let greet = function () {
+//   console.log("hello");
+// };
+// multipleGreet(greet, 20);
+
+// check odd-even
+// function oddOrEvenFactory(request) {
+//   if (request == "odd") {
+//     return function (n) {
+//       console.log(!(n % 2 == 0));
+//     };
+//   } else if (request == "even") {
+//     return function (n) {
+//       console.log(n % 2 == 0);
+//     };
+//   } else {
+//     console.log("wrong request");
+//   }
+// }
+// let request = "odd"; //even
+
+// Methods----------------------------
+const calculator = {
+  num: 55,
+  add: function (a, b) {
+    return a + b;
+  },
+  sub: function (a, b) {
+    return a - b;
+  },
+  mul: function (a, b) {
+    return a * b;
+  },
 };
-console.log(sum);
