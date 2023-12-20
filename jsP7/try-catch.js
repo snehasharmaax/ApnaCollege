b647a2753884f82b5c2fafd5be18754bfff0924b
snehasharmaax <snehasharmaax@gmail.com>
2023-12-20 23:14:02 +0530

    try - catch in JS

---
+++ jsP7/try-catch.js
@@ -0,0 +1,14 @@
+console.log("sneha");
+console.log("sneha");
+
+// let a = 5;
+try {
+  console.log(a);
+} catch (e) {
+  console.log("caught an error.. a is not defined");
+  console.log(e);
+}
+
+console.log("sneha2");
+console.log("sneha2");
+console.log("sneha2");
