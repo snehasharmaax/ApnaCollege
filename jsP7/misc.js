701bd230607c74cfb12c05735eeccef8a2926b4c
snehasharmaax <snehasharmaax@gmail.com>
2023-12-22 23:13:23 +0530

    Many Method

---
+++ jsP7/misc.js
@@ -0,0 +1,35 @@
+// -------------------- Arrow Functions -----------------
+// Syntax :- (arg1, arg2 .. ) => {function definition}
+
+// const sum = (a, b) => {
+//   console.log(a + b);
+// };
+
+// Explicit Return
+// const cube = (n) => {
+//   return n * n * n;
+// };
+
+// const pow = (a, b) => {
+//   return a ** b;
+// };
+
+// Implicit return
+// const mul = (a, b) => a * b;
+
+// -------------------- Set TimeOut -------------------
+// Syntax :- setTimeout(function, timeout)
+// console.log("Hi Sneha");
+// setTimeout(() => {
+//   console.log("Sneha Sharma");
+// }, 2000);
+// console.log("Welcome back");
+
+// -------------------- Set Interval -------------------
+// Syntax :- setInterval(function, timeout)
+// let id = setInterval(() => {
+//   console.log("Sneha Sharma");
+// }, 2000);
+// console.log(id);
+
+// -------------------- this with Arrow Functions
