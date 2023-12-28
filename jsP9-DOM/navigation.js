9af1b3110cb50a5f6731a5aba70ae36417e46a39
snehasharmaax <snehasharmaax@gmail.com>
2023-12-28 23:02:09 +0530

    Navigation in JS

---
+++ jsP9-DOM/navigation.js
@@ -0,0 +1,19 @@
+// Navigation :- parentElement, children, previousElementSibling / nextSibling
+
+// let h4 = document.querySelector("h4");
+// console.log(h4.parentElement);
+// console.log(h4.children);
+
+// let box = document.querySelector(".box");
+// console.log(box.childElementCount);
+// console.log(box.children);
+
+// let ul = document.querySelector("ul");
+// console.log(ul.parentElement);
+// console.log(ul.childElementCount);
+
+// console.log(ul.children[1].previousElementSibling);
+// console.log(ul.children[1].nextElementSibling);
+
+let img = document.querySelector("img");
+console.log((img.previousElementSibling.style.color = "green"));
