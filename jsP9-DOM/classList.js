98c090e555091155095d8ff52c1a68dc143e9620
snehasharmaax <snehasharmaax@gmail.com>
2023-12-28 23:01:17 +0530

    class List in JS

---
+++ jsP9-DOM/classList.js
@@ -0,0 +1,16 @@
+// Manipulating Style
+// using classList
+// obj.classList
+
+// classList.add() to add new classes
+// classList.remove() to remove classes
+// classList.contains() to check if class exists
+// classList.toggle() to toggle between add & remove
+
+let img = document.querySelector("img");
+// console.log(img.classList);
+
+let heading = document.querySelector("h1");
+console.log(heading.classList);
+
+heading.classList.add("abc");
