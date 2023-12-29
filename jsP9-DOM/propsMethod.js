8491dd8a4a2a5ebef104b0d2ecf04ef38981bf8b
snehasharmaax <snehasharmaax@gmail.com>
2023-12-29 23:52:55 +0530

    Props Mehtod in JS

---
+++ jsP9-DOM/propsMethod.js
@@ -0,0 +1,11 @@
+// Using Properties & Methods
+
+// innerText :- Shows the visible text contained in a node
+// textContent :- Shows all the full text
+// innerHTML :- Shows the full markup
+
+let para = document.querySelector("p");
+// console.dir(para);
+// console.dir(para.innerText);
+// console.dir(para.innerHTML);
+console.log(para.textContent);
