90c40ccd724efd3e1a0ece9a1aef0d2b197bd395
snehasharmaax <snehasharmaax@gmail.com>
2023-12-29 23:54:33 +0530

    Query Selector

---
+++ jsP9-DOM/querySelectors.js
@@ -0,0 +1,11 @@
+// Query Selectors :- Allows us to use any CSS selector
+// document.querySelector("p"); //Selects first p element
+// document.querySelector("#myId"); // Selects first element with id = myId
+// document.querySelector(".myClass"); // Selects first element with class = myClass
+// document.querySelectorAll("p"); //Selects all p elements
+
+// console.dir(document.querySelector("h1"));
+// console.dir(document.querySelector("#description"));
+// console.dir(document.querySelector(".oldImg"));
+
+console.dir(document.querySelectorAll("div a"));
