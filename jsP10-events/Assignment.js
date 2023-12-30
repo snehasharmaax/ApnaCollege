b8a58ea4ea683a8cc2b3b2d3cae05b12f0abb34a
snehasharmaax <snehasharmaax@gmail.com>
2023-12-30 23:25:13 +0530

    Assignment in JS Events

---
+++ jsP10-events/Assignment.js
@@ -0,0 +1,11 @@
+// Q.1 Try out the following events in Event Listener on your own:-
+// - mouseout
+// - keypress
+// - scroll
+// - load
+
+// Q.2 Create a button on the page using JS. Add an event listener to the button that changes the button's color to green when it is clicked.
+
+// Q.3 Create an input element on the page with a placeholder "enter your name" and an h2 heading on the page inside HTML.
+// The purpose of this input element is to enter a user's name so it should only input letters form a-z, A-Z and space (all other characters should not be detected).
+// Whenever the user inputs thier name, their input should be dynamically visible inside the heading.
