22a6c18a755b744530f9e9a34f17d372956b7125
snehasharmaax <snehasharmaax@gmail.com>
2024-01-02 22:53:33 +0530

    Async & Await concept in JS

---
+++ jsP12-async/asyncAwait.js
@@ -0,0 +1,63 @@
+// async :-----------------------------
+// async function greet() {
+//   // throw "some random error";
+//   return "hello";
+// }
+// greet()
+//   .then((result) => {
+//     console.log("promise was resolved");
+//     console.log("result was : ", result);
+//   })
+//   .catch((err) => {
+//     console.log("promise was rejected with err : ", err);
+//   });
+
+// await :- pauses the execution of its surrounding async function until the promise is settled( resolved or rejected)
+function getNum() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      let num = Math.floor(Math.random() * 10) + 1;
+      console.log(num);
+      resolve();
+    }, 1000);
+  });
+}
+
+async function demo() {
+  await getNum();
+  await getNum();
+  await getNum();
+}
+
+// Example -----------------
+h1 = document.querySelector("h1");
+
+function changeColor(color, delay) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      let num = Math.floor(Math.random() * 5) + 1;
+      if (num > 3) {
+        reject("promise rejected");
+      }
+
+      h1.style.color = color;
+      console.log(`color changed to ${color}!`);
+      resolve("color changed!");
+    }, delay);
+  });
+}
+
+async function colorDemo() {
+  try {
+    await changeColor("red", 1000);
+    await changeColor("orange", 1000);
+    await changeColor("green", 1000);
+    await changeColor("blue", 1000);
+  } catch (err) {
+    console.log(err);
+  }
+
+  let a = 5;
+  console.log(a);
+  console.log(`the number is : ${a + 3}`);
+}
