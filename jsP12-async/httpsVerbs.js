a6125c0da3d1c86980148ccb5f9497ecf83176f3
snehasharmaax <snehasharmaax@gmail.com>
2024-01-03 23:24:49 +0530

    https verbs

---
+++ jsP12-async/httpsVerbs.js
@@ -0,0 +1,18 @@
+// http Verbs : -----------------------------
+// - GET
+// - POST
+// - DELETE
+
+// Status Codes :
+// - 200 : OK
+// - 404 : Not Forund
+// - 400 : Bad Request
+// - 500 : Internal Server Error
+
+// Add Information in URLs
+// Query Strings :-
+// https://www.google.com/search?q=harry+potter
+// Query = ?name=sneha&marks=95
+
+// http headers ------------------------------
+// header, value
